66c7a55681bfcf72966e2180203b4143c35ad163
freegatik <anton.soloviev2016@gmail.com>
2026-04-03 10:47:31 +0300

    refactor(P2P-3): remove launch screen storyboard and related assets, simplifying app launch process

--- P2PMessenger/P2PMessengerApp.swift
+++ P2PMessenger/P2PMessengerApp.swift
@@ -12,60 +12,7 @@ import SwiftData
 struct P2PMessengerApp: App {
     var body: some Scene {
         WindowGroup {
-            AppRootView()
-        }
-    }
-}
-
-// MARK: - Корневой контейнер запуска приложения
-
-private struct AppRootView: View {
-    @State private var showsSplashScreen = true
-
-    var body: some View {
-        ZStack {
             ContentView()
-                .opacity(showsSplashScreen ? 0 : 1)
-
-            if showsSplashScreen {
-                LaunchSplashView()
-                    .transition(.opacity)
-            }
         }
-        .task {
-            try? await Task.sleep(for: .milliseconds(AppLaunchConstants.splashDelayMilliseconds))
-
-            withAnimation(.easeOut(duration: AppLaunchConstants.splashFadeDuration)) {
-                showsSplashScreen = false
-            }
-        }
-    }
-}
-
-// MARK: - Брендированный splash-экран поверх системного launch screen
-
-private struct LaunchSplashView: View {
-    var body: some View {
-        VStack(spacing: AppLaunchConstants.splashVerticalSpacing) {
-            Image(AppLaunchConstants.logoAssetName)
-                .resizable()
-                .scaledToFit()
-                .frame(
-                    width: AppLaunchConstants.splashLogoSize,
-                    height: AppLaunchConstants.splashLogoSize
-                )
-
-            Text(AppLaunchConstants.brandTitle)
-                .font(
-                    .system(
-                        size: AppLaunchConstants.splashTitleFontSize,
-                        weight: .regular,
-                        design: .rounded
-                    )
-                )
-                .foregroundStyle(Color.p2pTextPrimary)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.p2pBackground.ignoresSafeArea())
     }
 }
